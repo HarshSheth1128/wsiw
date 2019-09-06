a4c225c658b95f900c0939aecdcb9286c5941e8b
Anjali Notaney <anjalinotaney@hotmail.com>
2019-09-06 19:27:53 -0400

    Updated MoviePageClass state to contain variables

--- src/MovieViewPage/MovieViewPage.jsx
+++ src/MovieViewPage/MovieViewPage.jsx
@@ -5,7 +5,15 @@ import MoviePoster from "./../images/BigMoviePoster.png"
 class MovieViewPage extends Component {
     constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+          Movie: 'Star Wars',
+          MoviePosterUrl: MoviePoster,
+          MovieDuration: "120 min",
+          MovieReleaseYear: "(2009)",
+          MovieGenres: new Array(12).fill("Action"),
+          MovieDescription: "Summary... From Lucasfilm comes the first of the Star Wars standalone films, “Rogue One: A Star Wars Story,” an all-new epic adventure.",
+          CurrentTab: "CastAndCrew",
+      };
     }
 
     render(){
@@ -13,88 +21,49 @@ class MovieViewPage extends Component {
             <div className="MovieViewPage">
                 <div className="MovieViewModal">
                     <div className="ModalContentWrapper">
-                    <div className="ColumnsContentWrapper">
-                        <div className="LeftColumnWrapper">
-                        <img src={MoviePoster} style={{border: "thin solid #ee6055", height: 180}}/>
-                        </div>
-                        <div className="RightContentWrapper"> 
-                            <div className="HeadingWrapper"> 
-                                <h1 className="MovieTitle">
-                                    {"Star Wars"}
-                                </h1>   
-                                <h4 className="SecondaryHeadings" >
-                                {"(2009)"}
-                                </h4>
-                                <h4 className="SecondaryHeadings" >
-                                {"duration: 120min"}
-                                </h4>
+                        <div className="ColumnsContentWrapper">
+                            <div className="LeftColumnWrapper">
+                                <img 
+                                    src={this.state.MoviePosterUrl} 
+                                    style={{border: "thin solid #ee6055", height: 180}}
+                                />
                             </div>
-                            <div className="GenreSection">
-                                <div className="GenreContentWrapper">
+                            <div className="RightContentWrapper"> 
+                                <div className="HeadingWrapper"> 
+                                    <h1 className="MovieTitle">{this.state.Movie}</h1>   
+                                    <h4 className="SecondaryHeadings">{this.state.MovieReleaseYear}</h4>
+                                    <h4 className="SecondaryHeadings" >{this.state.MovieDuration}</h4>
+                                </div>
+                                <div className="GenreSection">
                                     <div className="GenreContentWrapper">
-                                        <div className="GenreContentWrapper">
-                                            <h1 className="GenreTitle">
-                                                {"Genre"}
-                                            </h1>
-                                            <div className="GenreCategoriesWrapper">
+                                        <h1 className="GenreTitle">{"Genre"}</h1>
+                                        <div className="GenreCategoriesWrapper">
+                                            {this.state.MovieGenres.map(Genre => (
                                                 <h2 className="GenreCategories">
                                                     {"Action"}
                                                 </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                                <h2 className="GenreCategories">
-                                                    {"Action"}
-                                                </h2>
-                                            </div>
+                                            ))}   
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <p className="MovieDescriptionText">
-                                {"Summary...  From Lucasfilm comes the first of the Star Wars standalone films, “Rogue One: A Star Wars Story,” an all-new epic adventure."}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="TabsSection"> 
-                        <div className="TabContentWrapper">
-                            <div className="TabHeadingsWrapper">
-                                <h1 className="TabHeadings">
-                                    {"Cast and Crew"}
-                                </h1>
-                                <h1 className="TabHeadings">
-                                    {"Critiques"}
-                                </h1>
-                                <h1 className="TabHeadings">
-                                    {"Recommendations"}
-                                </h1>
+                                <p className="MovieDescriptionText">{this.state.MovieDescription}</p>
                             </div>
                         </div>
-                    </div>
+                        <div className="TabsSection"> 
+                            <div className="TabContentWrapper">
+                                <div className="TabHeadingsWrapper">
+                                    <h1 className="TabHeadings">
+                                        {"Cast and Crew"}
+                                    </h1>
+                                    <h1 className="TabHeadings">
+                                        {"Critiques"}
+                                    </h1>
+                                    <h1 className="TabHeadings">
+                                        {"Recommendations"}
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
