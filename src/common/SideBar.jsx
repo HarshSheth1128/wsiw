a7e7e7722ecd46ce5e2477bad9422138bf5f1233
Harsh Sheth <nyanman28@gmail.com>
2019-09-06 10:02:39 -0400

    Harsh Finished up sidebar

--- src/common/SideBar.jsx
+++ src/common/SideBar.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Puppy from '../assets/puppy.jpg';
 import './Sidebar.scss';
 import ChevronLeft from '../assets/chevronLeft.svg';
 
 class SideBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: 'nyanman28',
+      profilePicUrl: Puppy,
+      friends: new Array(10).fill({
+        username: 'anjali.notaney',
+        profilePicUrl: Puppy,
+      }),
+    };
   }
 
   render() {
@@ -25,6 +33,31 @@ class SideBar extends Component {
             src={ChevronLeft}
             alt="backButton"
           />
+        </div>
+        <div className="Profile">
+          <img
+            className="ProfilePic"
+            src={this.state.profilePicUrl}
+            alt="Profile image"
+          />
+          <h1 className="ProfileUsername">{this.state.username}</h1>
+        </div>
+        <div className="SideBarLine" />
+        {this.state.friends.map(friend => (
+          <div className="Profile">
+            <img
+              className="ProfilePic"
+              src={friend.profilePicUrl}
+              alt="Profile image"
+            />
+            <h1 className="ProfileUsername">{friend.username}</h1>
+          </div>
+        ))}
+        <div className="Profile">
+          <div className="PlusButton">
+            <span className="PlusText">+</span>
+          </div>
+          <h1 className="ProfileUsername">Add Friend</h1>
         </div>
       </div>
     );
