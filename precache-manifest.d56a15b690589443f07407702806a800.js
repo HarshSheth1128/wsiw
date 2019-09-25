26a86e7dc9ad50a8136b091b54a23d4c97309491
Harsh Sheth <nyanman28@gmail.com>
2019-09-25 09:37:27 -0400

    Updates

---
+++ precache-manifest.d56a15b690589443f07407702806a800.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38b0e7c8dfb76aceef33cebf54be505",
+    "url": "/wsiw/index.html"
+  },
+  {
+    "revision": "50e91e919873a0abee78",
+    "url": "/wsiw/static/css/main.33d1470f.chunk.css"
+  },
+  {
+    "revision": "c862998a6cc37bf513c3",
+    "url": "/wsiw/static/js/2.b6acd7cb.chunk.js"
+  },
+  {
+    "revision": "50e91e919873a0abee78",
+    "url": "/wsiw/static/js/main.673732f5.chunk.js"
+  },
+  {
+    "revision": "66c35d626a667baef825",
+    "url": "/wsiw/static/js/runtime~main.e2f2445d.js"
+  },
+  {
+    "revision": "77df44521001245755704a479bce1006",
+    "url": "/wsiw/static/media/BigMoviePoster.77df4452.png"
+  },
+  {
+    "revision": "abef1870a35266a9d9b6f9cea7e42c23",
+    "url": "/wsiw/static/media/Likes.abef1870.svg"
+  },
+  {
+    "revision": "37dbe149bac28fe2754c9c1c629c54f2",
+    "url": "/wsiw/static/media/MoviePoster.37dbe149.jpg"
+  },
+  {
+    "revision": "ccb2eda53fdb771dc5824610795c8f14",
+    "url": "/wsiw/static/media/Settings.ccb2eda5.svg"
+  },
+  {
+    "revision": "6c7b6c3154fdd5c277b7a7d1bc549101",
+    "url": "/wsiw/static/media/Unlike.6c7b6c31.svg"
+  },
+  {
+    "revision": "8a5a3ef59421c40268c64c7d6914f667",
+    "url": "/wsiw/static/media/Views.8a5a3ef5.svg"
+  },
+  {
+    "revision": "bd9601884804f5e1e39d436bc05b9f63",
+    "url": "/wsiw/static/media/blackPanther.bd960188.jpg"
+  },
+  {
+    "revision": "aca62c71a0ca907ff2240886ff3ddfbb",
+    "url": "/wsiw/static/media/breakingbad.aca62c71.jpg"
+  },
+  {
+    "revision": "cfb2fe79623d78c1ef86b8221c9e41c8",
+    "url": "/wsiw/static/media/chevron.cfb2fe79.svg"
+  },
+  {
+    "revision": "e4f8301f0bb6f1370f70c018b9d163f4",
+    "url": "/wsiw/static/media/chevronLeft.e4f8301f.svg"
+  },
+  {
+    "revision": "a3d1eb4df1535c4082fbbf4bcb476ee7",
+    "url": "/wsiw/static/media/firetruck.a3d1eb4d.jpg"
+  },
+  {
+    "revision": "dccc29984d21589db0ec5b10df29a395",
+    "url": "/wsiw/static/media/mahomes.dccc2998.jpg"
+  },
+  {
+    "revision": "6dcb00e37e3394d0483f451203a64e8c",
+    "url": "/wsiw/static/media/moonlight.6dcb00e3.jpg"
+  },
+  {
+    "revision": "f07adf7c931023d9eeb0f06ca232cf52",
+    "url": "/wsiw/static/media/natalie-portman.f07adf7c.jpg"
+  },
+  {
+    "revision": "9788d242fccf509de39144e4ab01da13",
+    "url": "/wsiw/static/media/plus.9788d242.svg"
+  },
+  {
+    "revision": "1d949708ddbf2c6cd856e419aed17d06",
+    "url": "/wsiw/static/media/poster.1d949708.jpg"
+  },
+  {
+    "revision": "912f62dd97274e04296e114e350b6e11",
+    "url": "/wsiw/static/media/puppy.912f62dd.jpg"
+  },
+  {
+    "revision": "d983705efe097541dc70f1492533a11e",
+    "url": "/wsiw/static/media/rdj.d983705e.jpg"
+  },
+  {
+    "revision": "1fa83a07af3c57d4fa70da21e7498794",
+    "url": "/wsiw/static/media/search.1fa83a07.svg"
+  }
+]);
